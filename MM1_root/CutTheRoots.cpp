476764bc39c649375c2e6077757babd19378a296
Turf1013 <496528674@qq.com>
2016-04-18 14:07:43 +0800

    add cutTheRoots thought

--- MM1_root/CutTheRoots.cpp
+++ MM1_root/CutTheRoots.cpp
@@ -1,15 +1,38 @@
 #include <algorithm>
 #include <cstdlib>
 #include <iostream>
+#include <cstring>
+#include <stdio>
 #include <map>
+#include <set>
+#include <cmath>
 #include <sstream>
 #include <vector>
-
 using namespace std;
+
+#define sti				set<int>
+#define stpii			set<pair<int, int> >
+#define mpii			map<int,int>
+#define vi				vector<int>
+#define pii				pair<int,int>
+#define vpii			vector<pair<int,int> >
+#define rep(i, a, n) 	for (int i=a;i<n;++i)
+#define per(i, a, n) 	for (int i=n-1;i>=a;--i)
+#define clr				clear
+#define pb 				push_back
+#define mp 				make_pair
+#define fir				first
+#define sec				second
+#define all(x) 			(x).begin(),(x).end()
+#define SZ(x) 			((int)(x).size())
+
+const int maxnp = 110
+const int maxrt = 105010;
+pii O[maxnp];
 
 class CutTheRoots {
 public:
-    vector<int> makeCuts(int NP, vector<int> points, vector<int> roots) {
+    vector<int> makeCuts_raw(int NP, vector<int> points, vector<int> roots) {
         // first NP points give coordinates of the bases of the plants, written as x, y
         // get x-coordinates of the bases, sort them, make cuts along y-axis to separate x-coordinates
         vector<int> xs(NP);
@@ -27,6 +50,14 @@ public:
         }
         return ret;
     }
+	
+	vi makeCuts_binary(int NP, vi points, vi roots) {
+		rep(i, 0, NP) {
+			O[i].fir = points[i<<1];
+			O[i].sec = points[i<<1|1];
+		}
+		
+	}
 };
 // -------8<------- end of solution submitted to the website -------8<-------
 
