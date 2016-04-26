e3edc5bf1876bb39fca48f0a63a7d49b4f497979
Turf1013 <496528674@qq.com>
2016-04-26 22:02:33 +0800

    add template of my own

---
+++ MM2_dna/dna.cpp
@@ -0,0 +1,365 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+	`Comment this line when subbmit`
+*/ 
+#define LOCAL_DEBUG
+#define DEBUG
+
+#ifdef LOCAL_DEBUG
+#include "log.h"
+#endif
+
+#define sti				set<int>
+#define stpii			set<pair<int, int> >
+#define mpii			map<int,int>
+#define vi				vector<int>
+#define vstr			vector<string>
+#define pii				pair<int,int>
+#define vpii			vector<pair<int,int> >
+#define rep(i, a, n) 	for (int i=a;i<n;++i)
+#define per(i, a, n) 	for (int i=n-1;i>=a;--i)
+#define clr				clear
+#define pb 				push_back
+#define mp 				make_pair
+#define fir				first
+#define sec				second
+#define all(x) 			(x).begin(),(x).end()
+#define SZ(x) 			((int)(x).size())
+
+
+
+class DNASequencing {
+public:
+	int initTest(int testDifficulty) {
+		return 0;
+	}
+
+	int preProcessing() {
+		return 0;
+	}
+
+	int passReferenceGenome(int chromatidSequenceId, const vector<string>& chromatidSequence) {
+		return 0;
+	}
+
+	vector<string> getAlignment(int N, double normA, double normS, const vector<string>& readName, const vector<string>& readSequence) {
+		vector<string> ret(N, "");
+		for(int i=0; i<N; ++i) {
+			string qname = "sim"+ to_string(1 + i/2) + '/' + ((i%2) ? '2' : '1');
+			ret[i] =  qname + ",20,1,150,+,0";
+		}
+		return ret;
+	}
+};
+
+//%-------------------------------------Cut from here-------------------------------------------
+
+/**
+	Constants from the problem statement
+*/
+const int MAX_DIF_DIST = 300;
+const double NORM_A_SMALL = -3.392;
+const double NORM_A_MEDIUM = -3.962;
+const double NORM_A_LARGE = -2.710;
+const double MAX_AUC = 0.999999;
+FILE* logout;
+
+#define LOGFILE "dna.log"
+#define result_map unordered_map<string, Result_t>
+
+/**
+	\brief Reuslt of the align
+*/
+typedef struct Result_t {
+	int id;			/* ChromatidSequenceId */
+	int st;			/* Start position */
+	int ed; 		/* End position */
+	char strand;	/* strand */
+} Result_t;
+
+
+/**
+	\brief Match of the align
+*/
+typedef struct Match_t {
+	double conf;	/* condfidence */
+	int r;			/* 0 or 1 */
+} Match_t;
+
+
+/**
+	\brief Parse the string of result
+	\param 	line: the line to parse
+			res: the infomation to check matched
+			match: the infomation to calculate the match
+*/
+void parse(const string& line, Result_t& res, Match_t& match) {
+	int len = line.length();
+	int i = 0, j = 0, cnt = 0;
+
+	while (1) {
+		if (i==len-1 || line[i]==',') {
+			str = line.substr(j, i-j);
+			if (cnt == 1) 		res.id = stoi(str);
+			else if (cnt == 2)	res.st = stoi(str);
+			else if (cnt == 3)	res.ed = stoi(str);
+			else if (cnt == 4)	res.strand = str[0];
+			else if (cnt == 5)	match.conf = stod(str);
+			++cnt;
+			j = i + 1;
+		}
+
+		if (++i >= len)
+			break;
+	}
+
+	#ifdef DEBUG
+	assert(cnt == 5);
+	#endif
+}
+
+void parse(const string& line, Result_t& res, String& readName) {
+	int len = line.length();
+	int i = 0, j = 0, cnt = 0;
+
+	while (1) {
+		if (i==len-1 || line[i]==',') {
+			str = line.substr(j, i-j);
+			if (cnt == 0)		readName = str;
+			else if (cnt == 1) 	res.id = stoi(str);
+			else if (cnt == 2)	res.st = stoi(str);
+			else if (cnt == 3)	res.ed = stoi(str);
+			else if (cnt == 4)	res.strand = str[0];
+			++cnt;
+			j = i + 1;
+		}
+
+		if (++i >= len)
+			break;
+	}
+
+	#ifdef DEBUG
+	assert(cnt == 5);
+	#endif
+}
+
+
+void parse(const string& line, Result_t& res, Match_t& match, String& readName) {
+	int len = line.length();
+	int i = 0, j = 0, cnt = 0;
+
+	while (1) {
+		if (i==len-1 || line[i]==',') {
+			str = line.substr(j, i-j);
+			if (cnt == 0)		readName = str;
+			else if (cnt == 1) 	res.id = stoi(str);
+			else if (cnt == 2)	res.st = stoi(str);
+			else if (cnt == 3)	res.ed = stoi(str);
+			else if (cnt == 4)	res.strand = str[0];
+			else if (cnt == 5)	match.conf = stod(str);
+			++cnt;
+			j = i + 1;
+		}
+
+		if (++i >= len)
+			break;
+	}
+
+	#ifdef DEBUG
+	assert(cnt == 5);
+	#endif
+}
+
+/**
+	\brief parse the content of `Ground truth`
+	\param	filename: name of the file, suffix with ".minisam"
+	\return a unordered_map[readName] = result
+*/
+result_map parse_ground(const string& filename) {
+	unordered_map<string, Result_t> ret;
+	ifstream fin(filename);
+	string line, readName;
+	Result_t res;
+
+	if (!fin.is_open()) {
+		perror("%s not exists", filename.c_str());
+		abort(0);
+	}
+
+	while (fin >> line) {
+		parse(line, res, readName);
+		#ifdef DEBUG
+		assert(ret.find(readName) == res.end());
+		#endif
+		ret[readName] = res;
+	}
+
+	return ret;
+}
+
+
+/**
+	\brief check the format of each line from output
+	\param	line: one line of output
+*/
+bool check_format(const string& line, const vi& chrId) {
+	Result res;
+	Math_t match;
+
+	parse(line, res, match);
+	/* check position \ge 1 */
+	if (res.st<1 || res.ed<1 || res.ed<res.st)	return false;
+	/* check strand */
+	if (res.strand!='+' && res.strand!='-')	return false;
+	/* check confidence */
+	if (match.conf<0 || match.conf>1)	return false;
+	/* check ChromatidSequenceId */
+	{
+		bool flag = false;
+		for (auto id : chrId) {
+			if(id == res.id) {
+				flag = true;
+				break;
+			}
+		}
+		if (!flag)	return false;
+	}
+
+	return true;
+}
+
+/**
+	\brief check the output of the algorithm
+	\param	ans: the output string of the algorithm
+			readNames: the name of the read as input
+*/
+bool check_ans(const vstr& ans, int N, string[] readNames) {
+	int sz = SZ(ans);
+	unordered_set<string> st;
+	string readName;
+
+	if (sz != N)	{
+		printf("|ans| != N\n");
+		return false;
+	}
+
+	rep(i, 0, sz) {
+		int idx = ans[i].find(',');
+		readName = ans[i].substr(0, idx);
+		if (!check_format(ans[i])) {
+			printf("ans has wrong format: [%s]\n", ans[i].c_str());
+			return false;
+		}
+		if (st.count(readName))	{
+			printf("ans contains reapted read\n");
+			return false;
+		}
+		st.insert(readName);
+	}
+
+	rep(i, 0, sz) {
+		if (!st.count(readNames[i])) {
+			printf("ans not contain [%s]\n", readNames[i].c_str());
+			return false;
+		}
+	}
+
+	return true;
+}
+
+
+/**
+	\brief calculate the match
+	\param	ans: the vector<string> of output from core algorithm
+			filename: the name of the file contains `Ground truth`
+	\return vector<Match_t>: confidence with R of output from core algorithm
+*/
+vector<Match_t> calcMatch(const vstr& ans, const string& filename) {
+	vector<Match_t> ret
+	Result_t res;
+	Match_t match;
+	String readName;
+	int sz = SZ(ans);
+	result_map res_map = parse_ground(filename);
+	result_map::iterator iter;
+	int correct = 0;
+
+	rep(i, 0, sz) {
+		parse(ans[i], res, match, readName);
+		iter = res_map.find(readName);
+		const Result_t& _res = iter->sec;
+		const int& r = match.r;
+		r = (	(res.id == _res.id) &&
+				(res.strand == _res.strand) &&
+				(abs(res.st-_res.st)<=MAX_DIF_DIST)
+			) 	?  1 : 0;
+		correct += r;
+		ret.pb(match);
+	}
+
+	printf("Number of correct answers: %d/%d = %.4lf\n", corrct, sz, correct*1.0/sz);
+
+	return ret;
+}
+
+/**
+	\breif calculate the `Accuracy` of the algorithm
+*/
+double calcAccuarcy(const double norm_a, const int N, const vector<Match_t>& vmatch) {
+
+}
+
+/**
+	\brief calculate the `Speed` of the algorithm
+*/
+double calcSpeed(const double norm_s, const double Time, const double TimeCutOff) {
+	return (1.0 - Time/TimeCutOff) / norm_s;
+}
+
+/**
+	\brief calculate the `Score` of the algorithm
+*/
+double calcScore(const double testNorm, const double accuracy, const double speed) {
+	return testNorm * accuracy * speed;
+}
+
+static void _test(int seed) {
+	
+}
+
+static void test(int testcase) {
+	bool visit[10];
+
+	memset(visit, false, sizeof(visit));
+	while (testcase) {
+		visit[testcase%10] = true;
+		testcase /= 10;
+	}
+
+	for (int i=1; i<=3; ++i)
+		_test(i);
+}
+
+static void init_log() {
+	logout = fopen(LOGFILENAME, "w");
+	if (!logout) {
+		perror("%s can not open.", LOGFILENAME);
+		abort();
+	}
+}
+
+static void close_log() {
+	fclose(logout);
+}
+
+int main(int argc, char **argv) {
+	int testcase = argc > 1 ? stoi(argv[1]) : 1;
+
+	init_log();
+	test(testcase);
+	close_log();
+
+	return 0;
+}
