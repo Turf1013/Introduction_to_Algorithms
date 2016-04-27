311964ce8d24c267cc28722235c6c0d9f8e08aba
Turf1013 <496528674@qq.com>
2016-04-27 17:04:43 +0800

    [MM-dna] more observe

--- MM2_dna/observe.cpp
+++ MM2_dna/observe.cpp
@@ -18,13 +18,13 @@ using namespace std;
 #define all(x) 			(x).begin(),(x).end()
 #define SZ(x) 			((int)(x).size())
 
-#define WINDOWS
+// #define WINDOWS
 // #define LINUX
 
 #ifdef WINDOWS
-	const string DATA_PREFIX = "I:/";
+	const string DATA_PREFIX = "I:/TCO-MM/";
 #else 
-	const string DATA_PREFIX = "~/Code/";
+	const string DATA_PREFIX = "~/Code/Introduction_to_Algorithms/MM2_dna/";
 #endif /* WINDOWS */
 
 struct TestCase_t {
@@ -34,19 +34,19 @@ struct TestCase_t {
 		fa1(fa1), fa2(fa2), minisam(minisam) {}
 };
 
-const string READ_PATH = DATA_PREFIX + "TCO-MM/example/";
+const string READ_PATH = DATA_PREFIX + "example/";
 const string REFE_PATH = READ_PATH;
-TestCase_t small (
+TestCase_t smallCase (
 	READ_PATH + "small5.fa1", 
 	READ_PATH + "small5.fa2", 
 	READ_PATH + "small5.minisam"
 );
-TestCase_t medium (
+TestCase_t mediumCase (
 	READ_PATH + "medium5.fa1", 
 	READ_PATH + "medium5.fa2", 
 	READ_PATH + "medium5.minisam"
 );
-TestCase_t large (
+TestCase_t largeCase (
 	READ_PATH + "large5.fa1",
 	READ_PATH + "large5.fa2", 
 	READ_PATH + "large5.minisam"
@@ -88,7 +88,7 @@ string getReverseComplement(const string& s) {
 	int len = SZ(s);
 	string ret;
 	
-	rep(i, 0, len) ret.pb(getComplement(s[i]));
+	per(i, 0, len) ret.pb(getComplement(s[i]));
 	
 	return ret;
 }
@@ -176,11 +176,146 @@ void getEntry(const string& filename) {
 }
 
 /**
+	\brief init
+*/
+void init(const testCase_t& tcase) {
+	getRead(tcase.fa1, vread[0]);
+	getRead(tcase.fa2, vread[1]);
+	getEntry(tcase.minisam);
+}
+
+/**
+	\brief check the entry[-]'s string equal to his `Reverse Complement`
+*/
+bool check_RC(const Entry_t& entry) {
+	assert(entry.faId>=0 && entry.faId<2);
+	assert(entry.simId>=0 && entry.simId<SZ(vread[entry.faId]));
+
+	const string& org = vread[entry.faId][entry.simId];
+	string tmp = getReverseComplement(org);
+
+	return org == tmp;
+}
+
+/**
+	\brief obsevation the [-]是否表示原串的Reverse Complement
+*/
+void observe_RC() {
+	int sz = SZ(vent);
+	int tot = 0, correct = 0;
+
+	rep(i, 0, sz) {
+		const Entry_t& entry = vent[i];
+		if (entry.strand == '-') {
+			if (check_RC(entry))
+				++correct;
+			++tot;
+		}
+	}
+
+	printf("observe-RC [-]: tot = %d, correct = %d\n", tot, correct);
+}
+
+void observe_strand() {
+	int sz = SZ(vent);
+	bool flag = true;
+
+	for (int i=0; i<sz; i+=2) {
+		const Entry_t& entry_1 = vent[i];
+		const Entry_t& entry_2 = vent[i+1];
+		if (entry_1.strand == entry_2.strand) {
+			flag = false;
+			break;
+		}
+	}
+
+	printf("observe-strand [+/-]: %s", flag ? "YES":"NO");
+}
+
+void observe_positionLen() {
+	int sz = SZ(vent);
+	vi vc;
+
+	for (int i=0; i<sz; ++i) {
+		const Entry_t &entry = vent[i];
+		vc.pb(entry.to - entry.from);
+	}
+
+	sort(all(vc));
+	int n = unqiue(all(vc)) - vc.begin();
+	if (n == 1) {
+		printf("observe-positionLen : all equal, %d\n", vc[0]);
+	} else {
+		printf("observe-positionLen : [%d, %d]\n", vc[0], vc[n-1]);
+	}
+}
+
+void observe_pairDis() {
+
+}
+
+void observe_refeUndef() {
+
+}
+
+void observe_alignScore() {
+
+}
+
+void observe_substrFreq() {
+	
+}
+
+/**
 	\brief	the info need to observe
 	\param	testDifficuly: 0-small, 1-medium, 2-large
 */
 void _observation(int testDifficulty) {
-	
+
+	/**
+		\step 0: init according testDifficuly
+	*/
+	if (testDifficulty == 0)
+		init(smallCase);
+	else if (testDifficulty == 1)
+		init(mediumCase);
+	else
+		init(largeCase);
+
+	/**
+		\step 1: 是否一个为[+],另一个为[-]
+	*/
+	observe_strand();
+
+	/**
+		\step 2: 长度是否一定为150
+	*/
+	observe_positionLen();	
+
+	/**
+		\step 3: read对的间距情况
+	*/
+	observe_pairDis();
+
+	/**
+		\step 4: [-] presents `Reverse Complement` ???
+	*/
+	observe_RC();
+
+	/**
+		\step 5: reference undef
+	*/
+	observe_refeUndef();
+
+	/**
+		\step 6: align score
+	*/
+	observe_alignScore();
+
+	/**
+		\step 7: count substring's frequency
+	*/
+	observe_substrFreq();
 }
 
 void observation(int testCase) {
@@ -193,24 +328,18 @@ void observation(int testCase) {
 	}
 	
 	rep(i, 1, 4) {
-		_observation(i);
+		_observation(i - 1);
 	}
 }
 
 int main(int argc, char **argv) {
 	ios::sync_with_stdio(false);
 	
+	freopen("data.in", "r", stdin);
+
 	int testCase = (argc>1) ? stoi(argv[1]) : 1;
 	
-	// observation(testCase);
-	
-	Entry_t entry;
-	string line;
-	
-	cin >> line;
-	parseEntry(line, entry);
-	entry.print();
-	
+	observation(testCase);
 	
 	return 0;
 }
