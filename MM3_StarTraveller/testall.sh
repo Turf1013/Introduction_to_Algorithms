6f3201e24b9d4b640379c1b359cf57e11f98fd10
Turf1013 <496528674@qq.com>
2016-05-25 19:12:58 +0800

    [MM-Star] with a little lower algorithm

--- MM3_StarTraveller/testall.sh
+++ MM3_StarTraveller/testall.sh
@@ -3,11 +3,11 @@
 if [ -z "$1" ]; then
 	for i in {1..10}
 	do
-	   java -jar tester.jar -exec ./star -seed $i
+	   java -jar tester.jar -exec ./star -seed $i -save star$i.png
 	done
 else
 	for i in {1..10}	
 	do
-	   java -jar tester.jar -exec ./$1 -seed $i
+	   java -jar tester.jar -exec ./$1 -seed $i -save base$i.png
 	done
 fi
