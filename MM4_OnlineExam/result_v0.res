0c0f499afb73085b1e167527a554260422a6e326
Turf1013 <496528674@qq.com>
2016-06-15 17:13:07 +0800

    [exam] naive algorithm finish

---
+++ MM4_OnlineExam/result_v0.res
@@ -0,0 +1,10 @@
+----> 
+SLICE = 51, tot = 55654, avg = 562.162
+SLICE = 47, tot = 58555, avg = 591.465
+SLICE = 45, tot = 60443, avg = 610.535
+SLICE = 43, tot = 61543, avg = 621.646
+SLICE = 41, tot = 63736, avg = 643.798
+SLICE = 39, tot = 63204, avg = 638.424
+SLICE = 37, tot = 61867, avg = 624.919
+SLICE = 35, tot = 60816, avg = 614.303
+SLICE = 33, tot = 59069, avg = 596.657
---
+++ MM4_OnlineExam/show.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import os
+
+
+def extract(filename):
+	if not os.path.isfile(filename):
+		raise ValueError, "%s is unvalid filename" % (filename)
+	with open(filename, "r") as fin:
+		# skip first line
+		lines = fin.readlines()[1:]
+		ret = []
+		for line in lines:
+			line = line.strip()
+			if line:
+				L = line.split()
+				ret.append( L[-1] )
+		return ret
+
+		
+def getVid(s):
+	return int( s[s.rindex('v')+1:-4] )
+	
+		
+		
+def show(vid, ncase=99):
+	if isinstance(vid, int):
+		filename = "result_v%d.res" % (vid)
+	else:
+		filename = vid
+	resList = map(int, extract(filename))
+	score = sum(resList)
+	vid = getVid(filename)
+	print "%d: tot = %d, avg = %.3lf" % (vid, score, score*1.0/ncase)
+	
+		
+	
+def showAll(filepath="./"):
+	nameList = filter(lambda s:s.endswith(".res"), os.listdir())
+	for name in nameList:
+		filename = os.path.join(filepath, name)
+		show(name)
+		
+
+if __name__ == "__main__":
+	show(0)
